658167efb6d65e74934bcd7541d1d281b245cc63
Nick Kelley <125415952+nksz6@users.noreply.github.com>
2025-05-17 17:54:06 -0500

    added more examples

--- lambdaex/src/main/java/com/nk/java/LambdaExamples2.java
+++ lambdaex/src/main/java/com/nk/java/LambdaExamples2.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 public class LambdaExamples2 {
     public static void main(String[] args) throws IOException {
 
+        //Java Lambda expression can implement interfaces with
+        // a single unimplemented method (abstract) method,
+        //but as many default of static methods as you like.
+        //otherwise called functional interfaces.
+
+
         //lambda
         MyInterface MyInterface = (String text) -> {
             System.out.println(text);
@@ -16,6 +22,8 @@ public class LambdaExamples2 {
         MyInterface.printIt("hello world");
 
         MyInterface.printUtf8To("hello file", new FileOutputStream("data.txt"));
+
+        com.nk.java.MyInterface.printItToSystemOut("hello system out");
 
     }
 }
--- lambdaex/src/main/java/com/nk/java/MyInterface.java
+++ lambdaex/src/main/java/com/nk/java/MyInterface.java
@@ -6,17 +6,17 @@ import java.io.OutputStream;
 public interface MyInterface {
     void printIt(String text);
 
-default public void printUtf8To(String text, OutputStream outputStream) {
-    try {
-        outputStream.write(text.getBytes("UTF-8"));
-    } catch (IOException e) {
-        throw new RuntimeException("Error writing String as UTF-8 to OutputStream", e);
+    default public void printUtf8To(String text, OutputStream outputStream) {
+        try {
+            outputStream.write(text.getBytes("UTF-8"));
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing String as UTF-8 to OutputStream", e);
+        }
     }
-}
 
-static void printItToSystemOut(String text) {
-    System.out.println(text);
-}
+    static void printItToSystemOut(String text) {
+        System.out.println(text);
+    }
 
 
 
